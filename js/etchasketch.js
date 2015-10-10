8dbea49a09be023997ca0685dd9c8d59e2b7882d
Bas Langenberg <baslangenberg@gmail.com>
2015-10-10 17:51:24 +0200

    Add draw method

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,14 +1,21 @@
 // Refactor me! :-)
-var width = 50;
-var height = 50;
+var width = 25;
+var height = 15;
 var count = 0;
 
 $(document).ready(function() {
-  for(i=0;i<height;i++){
+  for(i=0;i<width;i++){
     $('#base').append('<div class="row" id="rown' + count + '"></div>');
-    for(x=0;x<width;x++){
+    for(x=0;x<height;x++){
       $('#rown'+count).append('<div class="pixel"></div>');
     }
     count++
   }
+
+  $('.pixel').hover(
+    function(){
+      $(this).addClass('mpixel').fadeTo('fast');
+    }
+  )
+
 });
