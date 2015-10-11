7aad6d7ba0f10443370228fd3fe33279de104d62
Bas Langenberg <baslangenberg@gmail.com>
2015-10-11 16:28:38 +0200

    Add dynamic size of pixels, based on the nbr req
    The pixels get generated within a 800 / 800 grid, and the size of each
    individual pixel is calculated to fit within said grid.

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-  $('.pixel').hover(
-    function(){
-      $(this).addClass('mpixel').fadeTo('fast');
-    }
-  )
-
   $('#reset').click(function() {
     clear();
   })
@@ -19,17 +13,25 @@ function clear() {
   console.log('cleared grid..');
 }
 
+$(document).on("mouseenter", ".pixel", function(){
+  $(this).addClass('mpixel').fadeTo('fast');
+})
+
 function draw() {
   $('#base').empty();
-
+  // Add debug code for too big numbers & empty values
   var height = document.getElementById('vheight').value;
   var width = document.getElementById('vwidth').value;
   var count = 0;
 
+  // -2 on width and heigth for the border...
+  dwidth = Math.floor(( 800 / width ) -2);
+  dheight = Math.floor(( 800 / height ) -2);
+
   for(i=0;i<width;i++){
     $('#base').append('<div class="row" id="rown' + count + '"></div>');
     for(x=0;x<height;x++){
-      $('#rown'+count).append('<div class="pixel"></div>');
+      $('#rown'+count).append('<div class="pixel" style=" width: '+dwidth+'px; height: '+dheight+'px;"></div>');
     }
     count++
   }
