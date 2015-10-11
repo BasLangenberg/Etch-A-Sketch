296f046e11b56f33e6a1eef42651023d2f32c5f7
Bas Langenberg <baslangenberg@gmail.com>
2015-10-11 14:44:16 +0200

    Remove border of base, refactor draw
    In order to get the draw function more generic, the following changes
    are made to the code:
    
     - No automatic grid will be drawn
     - Draw code is removed from document-ready

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,8 +1,3 @@
-// Refactor me! :-)
-var width = 25;
-var height = 15;
-var count = 0;
-
 $(document).ready(function() {
   for(i=0;i<width;i++){
     $('#base').append('<div class="row" id="rown' + count + '"></div>');
@@ -18,4 +13,23 @@ $(document).ready(function() {
     }
   )
 
-});
+  $('#reset').click(function() {
+    clear();
+  })
+})
+
+function clear() {
+  $('.pixel').removeClass('mpixel');
+  console.log('cleared grid..');
+}
+
+function draw() {
+  var count = 0;
+  for(i=0;i<width;i++){
+    $('#base').append('<div class="row" id="rown' + count + '"></div>');
+    for(x=0;x<height;x++){
+      $('#rown'+count).append('<div class="pixel"></div>');
+    }
+    count++
+  }
+}
